751a892d2dd48aab7da9aa88db93217bae49770a
ccalistru <cosmin.calistru@argus-presse.fr>
2018-11-15 14:13:20 +0200

    Swagger config

--- trombinoscope-webapp/src/main/java/hgrup/trombi/config/WebAppConfig.java
+++ trombinoscope-webapp/src/main/java/hgrup/trombi/config/WebAppConfig.java
@@ -3,13 +3,21 @@ package hgrup.trombi.config;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 @EnableWebMvc
 @ComponentScan(basePackages = { "hgrup.trombi"})
 public class WebAppConfig implements WebMvcConfigurer {
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("swagger-ui.html")
+                .addResourceLocations("classpath:/META-INF/resources/");
 
+        registry.addResourceHandler("/webjars/**")
+                .addResourceLocations("classpath:/META-INF/resources/webjars/");
+    }
     /*@Bean
     public InternalResourceViewResolver resolver() {
         InternalResourceViewResolver resolver = new InternalResourceViewResolver();
